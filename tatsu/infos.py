4448e83b17d37324bd4e35e60de182f0f1d321e1
Daniele Nicolodi <daniele@grinta.net>
2024-05-27 12:19:55 +0200

    lts: Tweaks for Python 3.8 compatibility

--- tatsu/infos.py
+++ tatsu/infos.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import copy
 import dataclasses
 import re
+import sys
 from collections.abc import Callable, MutableMapping
 from itertools import starmap
 from typing import Any, NamedTuple
@@ -11,6 +12,12 @@ from .ast import AST
 from .tokenizing import Tokenizer
 from .util.misc import cached_re_compile
 from .util.unicode_characters import C_DERIVE
+
+if sys.version_info < (3, 10):
+    import builtins
+
+    def zip(*iterables, strict=False):
+        return builtins.zip(*iterables)
 
 
 class UndefinedStr(str):
@@ -261,7 +268,7 @@ class RuleResult(NamedTuple):
     newstate: Any
 
 
-@dataclasses.dataclass(slots=True)
+@dataclasses.dataclass(**({'slots': True} if sys.version_info >= (3, 10) else {}))
 class ParseState:
     pos: int = 0
     ast: AST = dataclasses.field(default_factory=AST)
